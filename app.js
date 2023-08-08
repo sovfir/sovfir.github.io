beecfc9fb9eda955f2fbee02a0ad80a166a2c330
Sergei <85520080+sovfir@users.noreply.github.com>
2023-08-08 14:21:31 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -5,21 +5,29 @@ const screenDisplay = document.querySelector(".screen");
 let calculation = [];
 let clearCalc = [];
 
-function calculate(button) {
+function calculate(button) 
+{
   const value = button.textContent;
   if (value === "CLEAR") {
     calculation = [];
     clearCalc = [];
     screenDisplay.textContent = ".";
   } else if (value === "=") {
-    screenDisplay.textContent = eval(clearCalc);
+    let answer = eval(clearCalc);
+    answer = answer.toFixed(3)
+    screenDisplay.textContent = answer;
+    clearCalc.push(answer);
   } else {
     if (clearCalc.length <= 9) {
       calculation.push(value);
       clearCalc = calculation.join("");
       screenDisplay.textContent = clearCalc;
-      calculation = [];
-      calculation.push(clearCalc);
+    }
+    else
+    {
+        screenDisplay.textContent = ".";
+        calculation = [];
+        clearCalc = [];
     }
   }
 }
